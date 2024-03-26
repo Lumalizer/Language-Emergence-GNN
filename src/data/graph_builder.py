29faa99413a6633b1882c3057c035466bbe7afa4
Lumalizer <7863320+Lumalizer@users.noreply.github.com>
2024-03-26 14:44:06 +0100

    improve graph embeddings + builder

--- src/data/graph_builder.py
+++ src/data/graph_builder.py
@@ -13,7 +13,7 @@ class GraphBuilder(DatastringBuilder):
         super().__post_init__()
         assert self.embedding_size is not None
         # generate integer encodings for shapes and position types
-        self.pos_codes = {'above': 0, 'below': 1, 'left': 2, 'right': 3}
+        self.pos_codes = {'above': [1, 0, 0, 0], 'below': [0, 1, 0, 0], 'left': [0, 0, 1, 0], 'right': [0, 0, 0, 1]}
         self.shapes_codes = {s: i+1 for i, s in enumerate(sorted(self.shapes))}
         self.shapes_codes['origin'] = 0
         self.shapes_codes['0'] = -1
@@ -34,13 +34,15 @@ class GraphBuilder(DatastringBuilder):
             node_index = len(nodes)
             nodes.append(self.shapes_codes[shape])
             # above or below origin
-            edges.append((node_index, 0, self.pos_codes['above'] if i < 2 else self.pos_codes['below']))
-            # left or right of origin
-            edges.append((node_index, 0, self.pos_codes['right'] if i % 2 else self.pos_codes['left']))
+            updown = self.pos_codes['above'] if i < 2 else self.pos_codes['below']
+            leftright = self.pos_codes['right'] if i % 2 else self.pos_codes['left']
+            positions = [int((updown[i] + leftright[i]) == 1) for i in range(4)]
+
+            edges.append((node_index, 0, positions))
 
         x = torch.tensor(nodes, dtype=torch.float32).reshape([-1, 1])
         edge_index = torch.tensor([tuple(edge[:2]) for edge in edges], dtype=torch.int64).t().contiguous()
-        edge_attr = torch.tensor([edge[2] for edge in edges])
+        edge_attr = torch.tensor([edge[2] for edge in edges]).bool()
 
         data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
         return data
--- src/experiment/models/graph_embeddings.py
+++ src/experiment/models/graph_embeddings.py
@@ -8,19 +8,35 @@ class GraphEmbeddings(torch.nn.Module):
     def __init__(self, embedding_size):
         super(GraphEmbeddings, self).__init__()
         num_node_features = 1
-        self.emb1 = GATv2Conv(num_node_features, embedding_size, edge_dim=1, concat=False, heads=2)
-        self.bn1 = torch.nn.BatchNorm1d(embedding_size)
-        self.rel1 = torch.nn.ReLU()
+        self.emb1 = GATv2Conv(num_node_features, embedding_size*2, edge_dim=4, concat=False, heads=1)
+        self.emb2 = GATv2Conv(embedding_size*2, embedding_size, edge_dim=4, concat=False, heads=2)
+        self.emb3 = GATv2Conv(embedding_size, embedding_size, edge_dim=4, concat=False, heads=2)
 
-        self.lstmaggrr = aggr.LSTMAggregation(embedding_size, embedding_size)
+        self.bn1 = torch.nn.BatchNorm1d(embedding_size*2)
+        self.bn2 = torch.nn.BatchNorm1d(embedding_size)
+        self.bn3 = torch.nn.BatchNorm1d(embedding_size)
+
+        self.rel1 = torch.nn.ReLU()
+        self.rel2 = torch.nn.ReLU()
+        self.rel3 = torch.nn.ReLU()
+        
+        self.aggregate = aggr.GRUAggregation(embedding_size, embedding_size)
 
     def forward(self, d: Data) -> np.ndarray:
         node_features, edge_index, edge_attr, batch = d.x, d.edge_index, d.edge_attr, d.batch
-        edge_attr: torch.Tensor = edge_attr.float().unsqueeze(dim=1)
+        edge_attr: torch.Tensor = edge_attr.unsqueeze(dim=1)
 
         d = self.emb1(node_features, edge_index, edge_attr)
         d = self.bn1(d)
         d = self.rel1(d)
 
-        d = self.lstmaggrr(d, batch)
+        d = self.emb2(d, edge_index, edge_attr)
+        d = self.bn2(d)
+        d = self.rel2(d)
+
+        d = self.emb3(d, edge_index, edge_attr)
+        d = self.bn3(d)
+        d = self.rel3(d)
+
+        d = self.aggregate(d, batch)
         return d
